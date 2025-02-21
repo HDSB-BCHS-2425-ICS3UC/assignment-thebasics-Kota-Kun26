01eb5dff33424c73dd14d546593cb7366e316ead
Kota-Kun26 <shannon_colley@outlook.com>
2025-02-21 14:23:16 -0500

    MathOperators
    This is some of the basic python math operator functions.

---
+++ MathOperators.py
@@ -0,0 +1,28 @@
+import math
+#this is the square funtion#
+square=math.sqrt(25)
+print(square)
+#This is addition#
+sum_add=2+23
+print(sum_add)
+#This is subtraction#
+difference=2-23
+print(difference)
+#This is multiplication#
+product=2*3*4
+print(product)
+#This is division#
+quotient=31/8
+print(int(quotient))
+#This is 2 to the power of 4
+exponent=2**4
+print(exponent)
+""""
+These are remainders of division
+"""
+rem_1=24%6
+rem_2=24%5
+rem_3=24%7
+print(rem_1)
+print(rem_2)
+print(rem_3)
