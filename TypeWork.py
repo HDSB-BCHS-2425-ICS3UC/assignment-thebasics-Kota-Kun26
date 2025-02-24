80d31885fba320ce1c0eff33b5236925ab9d1666
Kota-Kun26 <shannon_colley@outlook.com>
2025-02-24 14:19:59 -0500

    TypeWork
    Declares each type of variable and prints it to the screen in the terminal.

---
+++ TypeWork.py
@@ -0,0 +1,27 @@
+#Kota Colley
+#2025/02/24
+
+#Integers
+print("This assigns x and y an integer. adds them and prints them to the screen")
+x=3
+y=2
+print(x+y)
+
+#Booleans
+print ("This is a Boolean that determines whether the logged in or logged out state is active")
+print ("This means that it is active, or true:")
+logged_in = True
+print("logged in")
+print ("This means that it is inactive, or false:")
+logged_out = True
+print("logged out")
+
+#Floats
+print("This prints a float of 0.5 to the screen")
+print(0.5)
+
+#Strings
+print("This prints a string of letters and symbols to the screen")
+print ("Ello :D")
+
+
